7d952371192725ff7b9a0fb10ec957c0d17c9c80
namiqmamedov <namigmm@code.edu.az>
2023-12-30 07:12:18 +0400

    Properties screen

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,7 +1,19 @@
 import prisma from "@/app/libs/prismadb"
 
+export interface IListingsParams {
+    userId?: string;
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    category?: string;
+  }
 
-export default async function getListings() {
+export default async function getListings(
+    params: IListingsParams
+) {
     try {
         const listings = await prisma.listing.findMany({
             orderBy: {
