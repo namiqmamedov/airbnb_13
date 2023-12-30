aed0cc8c57ea637c57ea6d6f215e76853a1eb5e9
namiqmamedov <namigmm@code.edu.az>
2023-12-30 10:21:15 +0400

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ interface HomeProps {
   searchParams: IListingsParams
 };
 
-export const dynamic = 'auto';
+export const dynamic = 'force-dynamic';
 
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
